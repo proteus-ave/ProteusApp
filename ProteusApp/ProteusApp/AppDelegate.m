cdf985e1b239a9611ad6c5f6f561d091c32f72d9
proteus-ave <proteus.ave@gmail.com>
2014-01-30 17:36:35 +0800

    view controller page

--- ProteusApp/ProteusApp/AppDelegate.m
+++ ProteusApp/ProteusApp/AppDelegate.m
@@ -10,9 +10,17 @@
 
 @implementation AppDelegate
 
+@synthesize window;
+@synthesize vc;
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.vc = [[ViewController alloc] init];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:self.vc];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 							
